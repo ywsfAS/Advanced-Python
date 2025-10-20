38b1d1420b7b73f7f13f56d86410195cdce29f47
Youssef AS <akhroub.youssf@gmail.com>
2025-10-20 23:10:12 +0100

    Update Exercice.py

--- Exercice.py
+++ Exercice.py
@@ -1,6 +1,6 @@
-with open("Table_de_multiplication.txt", "w+") as file:
+with open("Table_de_multiplication.txt", "w+", encoding="utf-8") as file:
     for a in range(1, 11):
-        file.write(f"Table de multiplication de {a:02}\n")
+        file.write(f"{'='*10} Table de multiplication de {a:02} {'='*10}\n")
         for b in range(1, 11):
-            file.write(f"{a:02} x {b:02} = {a * b}\n")
+            file.write(f"{a:02} x {b:02} = {a * b:02}\n")
         file.write("\n")
